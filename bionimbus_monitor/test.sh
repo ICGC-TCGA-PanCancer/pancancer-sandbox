479141f824bf2f12d5841bdd6060f94e746c137a
Brian O'Connor <boconnor@oicr.on.ca>
2014-09-23 15:54:56 -0500

    mod

--- bionimbus_monitor/test.sh
+++ bionimbus_monitor/test.sh
@@ -1,6 +1,8 @@
 #!/bin/bash
 
 cd /glusterfs/netapp/homes1/BOCONNOR/gitroot/pancancer-sandbox/bionimbus_monitor
-#  --setup-sensu 
+# first pass to reboot failed nodes
 perl test.pl --verbose --glob-base /glusterfs/netapp/homes1/BOCONNOR/gitroot/bindle_1.2.2 --glob-target 'target-os-c*'
+# run and setup sensu, hopefully will setup sensu on freshly rebooted nodes
+perl test.pl --verbose --glob-base /glusterfs/netapp/homes1/BOCONNOR/gitroot/bindle_1.2.2 --glob-target 'target-os-c*' --setup-sensu
 
