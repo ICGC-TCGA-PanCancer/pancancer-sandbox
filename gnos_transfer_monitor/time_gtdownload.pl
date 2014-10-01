5996db56dce0dc05fb950ce66c66cae9f34f9ca7
Brian O'Connor <briandoconnor@gmail.com>
2014-10-01 15:56:11 -0400

    Adding download testing

--- gnos_transfer_monitor/time_gtdownload.pl
+++ gnos_transfer_monitor/time_gtdownload.pl
@@ -3,6 +3,11 @@ use Data::Dumper;
 use Getopt::Long;
 use JSON;
 
+# NOTES
+# http://stackoverflow.com/questions/15233535/perl-regex-substitution-for-a-url
+# https://gtrepo-osdc-icgc.annailabs.com/cghub/data/analysis/download/441f1192-16ad-45cb-aefa-f4c3322a73dc
+
+
 #############
 #   USAGE   #
 #############
@@ -10,6 +15,7 @@ use JSON;
 if (scalar(@ARGV) < 6 || scalar(@ARGV) > 8) {
   die "USAGE: perl $0 --url <gnos_download_url> --url-file <url-file> --output <output.json> --pem <key_file.pem>"
 }
+
 
 #############
 # VARIABLES #
@@ -28,6 +34,69 @@ GetOptions(
   );
 
 
+
 ##############
 # MAIN STEPS #
 ##############
+
+# find all the URLs
+my $urls_hash = read_urls($url_file, \@urls);
+
+# run the individual URLs and report individual results
+my $url_runtimes = download_urls($urls_hash);
+
+# consolodate runtimes per site
+my $url_consol_runtimes = consolodate_runtimes($url_runtimes);
+
+# print report
+print_report($url_consol_runtimes, $output);
+
+
+
+###############
+# SUBROUTINES #
+###############
+
+sub read_urls {
+  my ($url_file, $urls) = @_;
+  my $d = {};
+  foreach my $url(@{$urls}) {
+    if ($url =~ m!^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?!) {
+      $d->{$url} = 1;
+    }
+  }
+  if (defined($url_file) && $url_file ne '' && -e $url_file) {
+    open IN, "<$url_file" or die;
+    while(<IN>) {
+      chomp;
+      if (m!^(([^:/?#]+):)?(//([^/?#]*))?([^?#]*)(\?([^#]*))?(#(.*))?!) {
+        $d->{$_} = 1;
+      }
+    }
+    close IN;
+  }
+  return($d);
+}
+
+sub download_urls {
+  my ($urls) = @_;
+  my $d = {};
+
+  foreach my $url (keys %{$urls}) {
+    $d->{$url} = gtdownload($url);
+  }
+}
+
+sub gtdownload {
+  my ($url) = @_;
+  my $d = {};
+  my $r = system("gtdownload ");
+}
+
+sub consolodate_runtimes {
+
+}
+
+sub print_report {
+
+}
