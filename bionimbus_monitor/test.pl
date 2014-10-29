e0dd486b895e7783d178d91c32f094a90d27b655
Brian O'Connor <briandoconnor@gmail.com>
2014-10-29 16:38:14 -0400

    Working on a pure nova version

--- bionimbus_monitor/test.pl
+++ bionimbus_monitor/test.pl
@@ -18,6 +18,7 @@ my ($glob_target) = @ARGV;
 my $test = 0;
 my $verbose = 0;
 my $setup_sensu = 0;
+my $use_nova = 0;
 my $glob_base = "";
 my $glob_target = "target-*";
 my $sensu_worker = "/glusterfs/netapp/homes1/BOCONNOR/gitroot/pancancer-sandbox/bionimbus_monitor/setup_sensu_worker.sh";
@@ -27,6 +28,10 @@ my $global_wait_time = 2;
 my $cleanup_jobs = 0;
 my $restart_workflows = 0;
 my $workflow_accession = 2;
+my $cluster_json = "cluster.json";
+my $pem = "/glusterfs/netapp/homes1/BOCONNOR/.ssh/brian-pdc-3.pem";
+my $username = "BOCONNOR";
+
 my $seqware_oozie_retry = '/glusterfs/netapp/homes1/BOCONNOR/gitroot/seqware-sandbox/seqware-oozie-restart/seqware-oozie-retry.pl';
 
 check_if_running();
@@ -43,7 +48,11 @@ GetOptions(
   "workflow-accession=i" => \$workflow_accession,
   "setup-sensu" => \$setup_sensu,
   "glob-base=s" => \$glob_base,
+  "use-nova" => \$use_nova,
   "glob-target=s" => \$glob_target,
+  "cluster-json=s" => \$cluster_json,
+  "ssh-pem=s" => \$pem,
+  "ssh-username=s" => \$username,
 );
 
 print "\n\n\n\n";
@@ -54,166 +63,225 @@ print "##############################\n";
 my $glob_path = $glob_target;
 if ($glob_base ne "") { $glob_path = "$glob_base/$glob_target"; }
 
-foreach my $target (glob($glob_path)) {
-  if (-d $target) {
-    my $master_ip;
-    my $cluster_name;
-    #next if (defined($glob_target) && $glob_target ne '' && $glob_target ne $target );
-    print "\n";
-    print "##############################\n";
-    print "# EXAMINING CLUSTER: $target #\n";
-    print "##############################\n";
-    foreach my $host ("$target/master", glob("$target/worker*")) {
-      if (-d $host) {
-        $host =~ /$target\/(\S+)/;
-        my $hostname = $1;
-        my $ssh_config = `cd $host && vagrant ssh-config 2> /dev/null`;
-        #print "SSH CMD: cd $host && vagrant ssh-config 2> /dev/null\n";
-        #print "SSH CONFIG: $ssh_config\n";
-        $ssh_config =~ /HostName\s+(\d+\.\d+\.\d+\.\d+)/;
-        my $curr_ip = $1;
-        $ssh_config =~ /User\s+(\S+)/;
-        my $curr_username = $1;
-        $ssh_config =~ /IdentityFile\s+(\S+)/;
-        my $curr_pem = $1;
+# can either use nova or a bindle target directory
+if ($use_nova) {
 
-        if ($hostname eq 'master') {
-            $master_ip = $curr_ip;
-            print "MASTER IP: $master_ip\n";
-            $target =~ /([^\/]+)$/;
-            $cluster_name = $1;
-        }
-        print "\n##############################\n";
-        print "#       HOST: $hostname      #\n";
-        print "##############################\n\n";
+  my $nova_str = `nova list | grep fleet_master | grep ACTIVE | grep Running | awk '{print \$2" "\$4" "\$12}'`;
+  if ($glob_target ne "") {
+    $nova_str = `nova list | grep $glob_target | grep ACTIVE | grep Running | awk '{print \$2" "\$4" "\$12}'`;
+  }
 
-        my $r = 0;
-        my $thr = threads->create(\&launch_ssh, "ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -i $curr_pem $curr_username\@$curr_ip hostname");
-        #print "ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -i $curr_pem $curr_username\@$curr_ip\n";
-        my $max_it = $global_max_it;
-        while($max_it > 0) {
-          print "SSH WAIT LOOP: $max_it\n";
-          sleep $global_wait_time;
-          if ($thr->is_joinable()) {
-            # then we can exit now
-            print "SSH THREAD FINISHED EARLY, BREAKING\n";
-            $max_it = 0;
+  open OUT, ">$cluster_json" or die;
+  print OUT "{\n";
+
+  my $first = 1;
+  my $i = 0;
+  # loop over each and test network connection
+  my @lines = split /\n/, $nova_str;
+  foreach my $line (@lines) {
+
+    $i++;
+
+    my ($uuid, $host, $ip) = split " ", $line;
+    $ip =~ /vmnet=(\S+)/;
+    $ip = $1;
+
+    # if network failed, stop the instance so it can be debugged later
+    my $r = test_ssh($pem, $username, $ip);
+
+    if ($r == 0) {
+      # add to cluster.json
+      if ($first) { $first = 0; }
+      else { print OUT ",\n"; }
+      print OUT <<END;
+  "cluster-$i": {
+     "workflow_accession": "2",
+     "workflow_name": "Workflow_Bundle_BWA",
+     "workflow_version": "2.6.0",
+     "username": "admin\@admin.com",
+     "password": "admin",
+     "webservice": "http://$ip:8080/SeqWareWebService",
+     "host": "master",
+     "max_workflows": "1",
+     "max_scheduled_workflows": "1"
+   }
+END
+    } else {
+      # stop the host
+      print "STOPPING UNREACHABLE HOST: $uuid $host $ip\n";
+      ######system("nova stop $uuid");
+    }
+
+  }
+
+  print OUT "}\n";
+  close OUT;
+
+} else {
+
+  # then look at the target directories instead from bindle
+
+  foreach my $target (glob($glob_path)) {
+    if (-d $target) {
+      my $master_ip;
+      my $cluster_name;
+      #next if (defined($glob_target) && $glob_target ne '' && $glob_target ne $target );
+      print "\n";
+      print "##############################\n";
+      print "# EXAMINING CLUSTER: $target #\n";
+      print "##############################\n";
+      foreach my $host ("$target/master", glob("$target/worker*")) {
+        if (-d $host) {
+          $host =~ /$target\/(\S+)/;
+          my $hostname = $1;
+          my $ssh_config = `cd $host && vagrant ssh-config 2> /dev/null`;
+          #print "SSH CMD: cd $host && vagrant ssh-config 2> /dev/null\n";
+          #print "SSH CONFIG: $ssh_config\n";
+          $ssh_config =~ /HostName\s+(\d+\.\d+\.\d+\.\d+)/;
+          my $curr_ip = $1;
+          $ssh_config =~ /User\s+(\S+)/;
+          my $curr_username = $1;
+          $ssh_config =~ /IdentityFile\s+(\S+)/;
+          my $curr_pem = $1;
+
+          if ($hostname eq 'master') {
+              $master_ip = $curr_ip;
+              print "MASTER IP: $master_ip\n";
+              $target =~ /([^\/]+)$/;
+              $cluster_name = $1;
           }
-          $max_it--;
-          #my $r = system("cd $host && vagrant ssh -c 'hostname' 2> /dev/null");
-        }
-        if ($max_it <= 0 && ($thr->is_running() || defined($thr->error()))) {
-          print "SSH THREAD STILL RUNNING OR IN ERROR: ".$thr->is_running()." ".$thr->error()."\n";
-          $r = 1;
-          # TODO: need to send the interrupt signal here
-          $thr->kill('KILL')->detach();
-          #threads->exit();
-        } elsif ($thr->is_joinable()) {
-          $thr->join();
-          $r = 0;
-        }
-        print "SSH RESULT: $r\n";
+          print "\n##############################\n";
+          print "#       HOST: $hostname      #\n";
+          print "##############################\n\n";
 
-        #print "  cd $host && vagrant ssh -c 'hostname' 2> /dev/null\n";
-        #print "  CMD STATUS: $r\n";
-        # need to restart, ssh doesn't work!
-        if ($r != 0) {
-          print "  REBOOTING HOST: $hostname\n";
-          my $ip = `cd $host && vagrant ssh-config | grep HostName | awk '{print \$2}' 2> /dev/null`;
-          chomp $ip;
-          if ($ip =~ /\d+\.\d+\.\d+\.\d+/) {
-            my $nova_id = `bash -l -c 'nova list | grep "$ip " | awk "{print \\\$2}"'`;
-            chomp $nova_id;
-            #print "  IP: $ip NOVA_ID $nova_id\n";
-            # reboot here
-            reboot_host($nova_id);
+          my $r = 0;
+          my $thr = threads->create(\&launch_ssh, "ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -i $curr_pem $curr_username\@$curr_ip hostname");
+          #print "ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -i $curr_pem $curr_username\@$curr_ip\n";
+          my $max_it = $global_max_it;
+          while($max_it > 0) {
+            print "SSH WAIT LOOP: $max_it\n";
+            sleep $global_wait_time;
+            if ($thr->is_joinable()) {
+              # then we can exit now
+              print "SSH THREAD FINISHED EARLY, BREAKING\n";
+              $max_it = 0;
+            }
+            $max_it--;
+            #my $r = system("cd $host && vagrant ssh -c 'hostname' 2> /dev/null");
           }
-        } else {
+          if ($max_it <= 0 && ($thr->is_running() || defined($thr->error()))) {
+            print "SSH THREAD STILL RUNNING OR IN ERROR: ".$thr->is_running()." ".$thr->error()."\n";
+            $r = 1;
+            # TODO: need to send the interrupt signal here
+            $thr->kill('KILL')->detach();
+            #threads->exit();
+          } elsif ($thr->is_joinable()) {
+            $thr->join();
+            $r = 0;
+          }
+          print "SSH RESULT: $r\n";
 
-          # now check the hostname
-          my $remote_name = `cd $host && vagrant ssh -c 'hostname' 2> /dev/null`;
-          #print "cd $host && vagrant ssh -c 'hostname' 2> /dev/null\n";
-          print "  REMOTE NAME: $remote_name\n";
-          chomp $remote_name;
-          $remote_name =~ /(\S+)/;
-          $remote_name = $1;
-          if ($remote_name ne $hostname) {
-            print "  RESETTING HOST: REMOTE: $remote_name LOCAL: $hostname\n";
-            reset_host($host, $hostname, $master_ip);
+          #print "  cd $host && vagrant ssh -c 'hostname' 2> /dev/null\n";
+          #print "  CMD STATUS: $r\n";
+          # need to restart, ssh doesn't work!
+          if ($r != 0) {
+            print "  REBOOTING HOST: $hostname\n";
+            my $ip = `cd $host && vagrant ssh-config | grep HostName | awk '{print \$2}' 2> /dev/null`;
+            chomp $ip;
+            if ($ip =~ /\d+\.\d+\.\d+\.\d+/) {
+              my $nova_id = `bash -l -c 'nova list | grep "$ip " | awk "{print \\\$2}"'`;
+              chomp $nova_id;
+              #print "  IP: $ip NOVA_ID $nova_id\n";
+              # reboot here
+              reboot_host($nova_id);
+            }
           } else {
-            print "  HOST OK\n";
-          }
 
-          # install sensu if specified
-          if ($setup_sensu && !$test) {
-            my $cmd = "cd $host && vagrant ssh -c 'sudo bash $sensu_worker $cluster_name $hostname $curr_ip' 2> /dev/null";
-            if ($hostname eq 'master') {
-              $cmd = "cd $host && vagrant ssh -c 'sudo bash $sensu_master $cluster_name $hostname $curr_ip' 2> /dev/null";
+            # now check the hostname
+            my $remote_name = `cd $host && vagrant ssh -c 'hostname' 2> /dev/null`;
+            #print "cd $host && vagrant ssh -c 'hostname' 2> /dev/null\n";
+            print "  REMOTE NAME: $remote_name\n";
+            chomp $remote_name;
+            $remote_name =~ /(\S+)/;
+            $remote_name = $1;
+            if ($remote_name ne $hostname) {
+              print "  RESETTING HOST: REMOTE: $remote_name LOCAL: $hostname\n";
+              reset_host($host, $hostname, $master_ip);
+            } else {
+              print "  HOST OK\n";
             }
-            my $r = system($cmd);
-            if ($r) { print "  SENSU INSTALL FAILED\n"; }
-          }
 
-          # checks to cleanup any SGE jobs on dead hosts
-          if ($cleanup_jobs && $hostname eq 'master') {
-            my $failed_nodes = {};
-            my $clean = `cd $host && vagrant ssh -c 'qstat -f'`;
-            my @lines = split /\n/, $clean;
-            foreach my $line (@lines) {
-              if ($line =~ /\s+au\s+/) {
-                $line =~ /main.q\@(\S+)/;
-                $failed_nodes->{$1} = 1;
+            # install sensu if specified
+            if ($setup_sensu && !$test) {
+              my $cmd = "cd $host && vagrant ssh -c 'sudo bash $sensu_worker $cluster_name $hostname $curr_ip' 2> /dev/null";
+              if ($hostname eq 'master') {
+                $cmd = "cd $host && vagrant ssh -c 'sudo bash $sensu_master $cluster_name $hostname $curr_ip' 2> /dev/null";
               }
-            }
-            print Dumper($failed_nodes);
-            # kill those jobs
-            my $kill_list = "";
-            $clean = `cd $host && vagrant ssh -c 'qstat'`;
-            @lines = split /\n/, $clean;
-            foreach my $line (@lines) {
-              $line =~ /main.q\@(\S+)/;
-              print "EXAMINED HOST: $1\n";
-              if ($failed_nodes->{$1}) {
-                $line =~ /^\s+(\d+)\s+/;
-                $kill_list .= " $1";
-              }
-            }
-            my $cmd = "cd $host && vagrant ssh -c 'sudo qdel -f $kill_list'";
-            print "KILL STUCK SGE JOBS: $cmd\n";
-
-            if (!$test && length($kill_list) > 0) {
               my $r = system($cmd);
-              if ($r) { print "PROBLEMS KILLING SGE JOBS!\n"; }
+              if ($r) { print "  SENSU INSTALL FAILED\n"; }
             }
-          }
 
-          # checks to restart failed workflows
-          if ($restart_workflows && $hostname eq 'master') {
-            print "RESTART WORKFLOWS: cd $host && vagrant ssh -c 'seqware workflow report --accession $workflow_accession'\n";
-            my $clean = `cd $host && vagrant ssh -c 'seqware workflow report --accession $workflow_accession'`;
-            my @lines = split /\n/, $clean;
-            my $accession;
-            my $status;
-            my $working_dir;
-            my $eid;
-            foreach my $line (@lines) {
-              if ($line =~ /Workflow Run SWID\s+\|\s+(\d+)/) { $accession = $1; }
-              elsif ($line =~ /Workflow Run Status\s+\|\s+(\S+)/) { $status = $1; }
-              elsif ($line =~ /Workflow Run Working Dir\s+\|\s+(\S+)/) { $working_dir = $1; }
-              elsif ($line =~ /Workflow Run Engine ID\s+\|\s+(\S+)/) { $eid = $1; }
-              elsif ($line =~ /Library Sample Names/ && $status eq 'failed') {
-                my $cmd = "cd $host && vagrant ssh -c 'perl $seqware_oozie_retry $working_dir $eid $accession'";
-                print "RESTARTING SEQWARE WORKFLOWS: $cmd\n";
-                if (!$test) {
-                  my $r = system($cmd);
-                  if ($r) {
-                    print "PROBLEMS RESTARTING WORKFLOW! '$seqware_oozie_retry $working_dir $eid $accession'\n";
+            # checks to cleanup any SGE jobs on dead hosts
+            if ($cleanup_jobs && $hostname eq 'master') {
+              my $failed_nodes = {};
+              my $clean = `cd $host && vagrant ssh -c 'qstat -f'`;
+              my @lines = split /\n/, $clean;
+              foreach my $line (@lines) {
+                if ($line =~ /\s+au\s+/) {
+                  $line =~ /main.q\@(\S+)/;
+                  $failed_nodes->{$1} = 1;
+                }
+              }
+              print Dumper($failed_nodes);
+              # kill those jobs
+              my $kill_list = "";
+              $clean = `cd $host && vagrant ssh -c 'qstat'`;
+              @lines = split /\n/, $clean;
+              foreach my $line (@lines) {
+                $line =~ /main.q\@(\S+)/;
+                print "EXAMINED HOST: $1\n";
+                if ($failed_nodes->{$1}) {
+                  $line =~ /^\s+(\d+)\s+/;
+                  $kill_list .= " $1";
+                }
+              }
+              my $cmd = "cd $host && vagrant ssh -c 'sudo qdel -f $kill_list'";
+              print "KILL STUCK SGE JOBS: $cmd\n";
+
+              if (!$test && length($kill_list) > 0) {
+                my $r = system($cmd);
+                if ($r) { print "PROBLEMS KILLING SGE JOBS!\n"; }
+              }
+            }
+
+            # checks to restart failed workflows
+            if ($restart_workflows && $hostname eq 'master') {
+              print "RESTART WORKFLOWS: cd $host && vagrant ssh -c 'seqware workflow report --accession $workflow_accession'\n";
+              my $clean = `cd $host && vagrant ssh -c 'seqware workflow report --accession $workflow_accession'`;
+              my @lines = split /\n/, $clean;
+              my $accession;
+              my $status;
+              my $working_dir;
+              my $eid;
+              foreach my $line (@lines) {
+                if ($line =~ /Workflow Run SWID\s+\|\s+(\d+)/) { $accession = $1; }
+                elsif ($line =~ /Workflow Run Status\s+\|\s+(\S+)/) { $status = $1; }
+                elsif ($line =~ /Workflow Run Working Dir\s+\|\s+(\S+)/) { $working_dir = $1; }
+                elsif ($line =~ /Workflow Run Engine ID\s+\|\s+(\S+)/) { $eid = $1; }
+                elsif ($line =~ /Library Sample Names/ && $status eq 'failed') {
+                  my $cmd = "cd $host && vagrant ssh -c 'perl $seqware_oozie_retry $working_dir $eid $accession'";
+                  print "RESTARTING SEQWARE WORKFLOWS: $cmd\n";
+                  if (!$test) {
+                    my $r = system($cmd);
+                    if ($r) {
+                      print "PROBLEMS RESTARTING WORKFLOW! '$seqware_oozie_retry $working_dir $eid $accession'\n";
+                    }
                   }
                 }
               }
             }
-          }
 
+          }
         }
       }
     }
@@ -246,6 +314,16 @@ sub reboot_host {
   }
 }
 
+sub stop_host {
+  my ($nova_id) = @_;
+  my $cmd = "bash -l -c 'nova stop $nova_id'";
+  if ($verbose) { print "    REBOOTING CMD: $cmd\n"; }
+  if (!$test) {
+    my $r = system($cmd);
+    if ($r != 0) { print "PROBLEMS REBOOTING HOST\n"; }
+  }
+}
+
 sub launch_ssh {
   $SIG{'KILL'} = sub { threads->exit(); };
   my $ssh = $_[0];
@@ -257,4 +335,38 @@ sub check_if_running {
   my $r = `ps aux | grep perl | grep $0 | grep -v grep | wc -l`;
   chomp $r;
   if ($r >1) { die "EXIT: more than one $0 is running!\n"; }
+}
+
+sub test_ssh {
+
+  my $r = 0;
+
+  my ($curr_pem, $curr_username, $curr_ip) = @_;
+
+  my $thr = threads->create(\&launch_ssh, "ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -i $curr_pem $curr_username\@$curr_ip hostname");
+  #print "ssh -o ConnectTimeout=10 -o BatchMode=yes -o StrictHostKeyChecking=no -i $curr_pem $curr_username\@$curr_ip\n";
+  my $max_it = $global_max_it;
+  while($max_it > 0) {
+    print "SSH WAIT LOOP: $max_it\n";
+    sleep $global_wait_time;
+    if ($thr->is_joinable()) {
+      # then we can exit now
+      print "SSH THREAD FINISHED EARLY, BREAKING\n";
+      $max_it = 0;
+    }
+    $max_it--;
+    #my $r = system("cd $host && vagrant ssh -c 'hostname' 2> /dev/null");
+  }
+  if ($max_it <= 0 && ($thr->is_running() || defined($thr->error()))) {
+    print "SSH THREAD STILL RUNNING OR IN ERROR: ".$thr->is_running()." ".$thr->error()."\n";
+    $r = 1;
+    # TODO: need to send the interrupt signal here
+    $thr->kill('KILL')->detach();
+    #threads->exit();
+  } elsif ($thr->is_joinable()) {
+    $thr->join();
+    $r = 0;
+  }
+  print "SSH RESULT: $r\n";
+  return($r);
 }
