583fa339bda8fbcfc04a77277418049ac5f2c584
Brian O'Connor <briandoconnor@gmail.com>
2014-10-29 17:41:42 -0400

    working on nova version

--- bionimbus_monitor/test_nova.sh
+++ bionimbus_monitor/test_nova.sh
@@ -2,4 +2,4 @@
 
 cd /glusterfs/netapp/homes1/BOCONNOR/gitroot/pancancer-sandbox/bionimbus_monitor
 # checks and turns off nodes it can't ssh to
-perl test.pl --verbose --use-nova --glob-target 'fleet_master' --cluster-json /glusterfs/netapp/homes1/BOCONNOR/gitroot/decider/decider-bwa-pancancer/pdc_cluster.2.json --ssh-pem /glusterfs/netapp/homes1/BOCONNOR/.ssh/brian-pdc-3.pem --ssh-username BOCONNOR --test
+perl test.pl --verbose --use-nova --glob-target 'fleet_master' --cluster-json /glusterfs/netapp/homes1/BOCONNOR/gitroot/decider/decider-bwa-pancancer/pdc_cluster.2.json --ssh-pem /glusterfs/netapp/homes1/BOCONNOR/.ssh/brian-pdc-3.pem --ssh-username BOCONNOR
