eb076e79ad98aa3437b5eff2d6e711af8033041a
Niall Byrne <Niall.Byrne@oicr.on.ca>
2015-06-27 13:06:29 -0400

    scheduling fixes

--- ops_scripts/scheduling/schedule_docker.py
+++ ops_scripts/scheduling/schedule_docker.py
@@ -211,6 +211,8 @@ def FeedMachines(ips, ini_files, key=SSHKEY_LOCATION):
     if len(ini_files) == 0:
         print "There are no more ini files available for scheduling."
         sys.exit(1)
+    
+    print ini_files
 
     for ip in ips:
         # Check if the Machine is Busy
