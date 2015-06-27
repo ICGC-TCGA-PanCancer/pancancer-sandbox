8e4ec297ef702a5f65c5330eacc1c123bb1228ce
Niall Byrne <Niall.Byrne@oicr.on.ca>
2015-06-27 15:50:47 -0400

    scheduling fixes

--- ops_scripts/orchestra/orchestra.py
+++ ops_scripts/orchestra/orchestra.py
@@ -81,6 +81,8 @@ def HostList():
     for ip in targets:
         if ip in busyhosts:
             container = LastContainer(ip)
+            if len(container.stip()) == 0:
+                container = "Unknown CID"
             result.append("%s\tCurrently running docker container: %s" % (ip, container))
         else:
             result.append("%s\tCurrently idle." % ip)
