423d2075dbaa2296db29d5c004d4f92233956513
Ubuntu <reading.crows@gmail.com>
2015-08-17 16:07:07 +0000

    .

--- ops_scripts/orchestra/auto-scheduler.sh
+++ ops_scripts/orchestra/auto-scheduler.sh
@@ -3,7 +3,7 @@
 # Schedules jobs on any current lazy workers
 # Workers with failed workflows will not be schduled to
 
-orchestra lazy | grep -v RESPONDING > .lazy_hosts
+/bin/orchestra lazy | grep -v RESPONDING > .lazy_hosts
 
 for x in `cat .lazy_hosts`; do
 	echo "Scheduling on $x" >> auto-scheduler.log
