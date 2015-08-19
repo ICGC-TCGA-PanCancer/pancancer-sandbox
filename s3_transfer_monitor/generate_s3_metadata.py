18a71808da22aee70dc57307beb5fab92b018869
Sheldon McKay <sheldon.mckay@gmail.com>
2015-08-19 10:28:09 +0000

    initial import of s3 timing JSON generator

---
+++ s3_transfer_monitor/generate_s3_metadata.py
@@ -0,0 +1,158 @@
+#!/usr/bin/env python
+
+import os
+import csv
+import json
+import shutil
+import datetime
+from subprocess import call, check_output
+from pprint import pprint as Dumper
+from glob import glob
+
+def day(delta):
+    """Get the formatted date of today or yesterday"""
+    if delta:
+        then = datetime.datetime.today() - datetime.timedelta(delta)
+        return then.strftime('%Y-%m-%d')
+    else:
+        return datetime.datetime.today().strftime('%Y-%m-%d')
+
+today     = day(0)
+yesterday = day(1)
+git_base       = '/mnt/data/s3-transfer-operations/'
+git_path       = git_base + 's3-transfer-jobs'
+timing_path    = git_base + 'timing-information'
+reports        = git_base + 's3-metadata'
+today_path     = reports  + '/' + today
+yesterday_path = reports  + '/' + yesterday
+
+def call_and_check(command):
+    """wraps system call with retval check"""
+    assert len(command) > 0, "command must not be empty!"
+    print "Making system call for '"+command+"'"
+    retval = call(command, shell=True)
+    assert retval == 0, "Failure executing command."
+
+def save_json(count_file, data):
+    """Saves count data to a json file"""
+    print "Saving JSON file "+count_file
+    csd = os.getcwd()
+    os.chdir(today_path)
+    json_string = json.dumps(data);
+    with open(count_file, 'w') as outfile:
+        outfile.write(json_string + "\n")
+    file_test(count_file)
+    os.chdir(cwd)
+    return count_file
+
+def file_test(count_file):
+    """Sanity check for empty or non-exsistent results"""
+    assert os.path.isfile(count_file), 'Error: '+count_file+' does not exists.'
+    assert os.stat(count_file).st_size > 0,'Error: '+count_file+' is empty.'
+
+
+def add_timepoint(timing,thing,repo,day,value):
+    """Adds time point to timing data by operation/repo"""
+    assert timing.keys() > 0, "This is not the timing hash I expected"
+    if day not in timing[thing].keys():
+        timing[thing][day] = {}
+    if repo not in timing[thing][day].keys():
+        timing[thing][day][repo] = [];
+    timing[thing][day][repo].append(value)
+
+# Once a day
+if os.path.exists(today_path):
+    shutil.rmtree(today_path, ignore_errors=True)
+os.makedirs(today_path)
+
+# Update data from github
+print "Updating s3 transfer data..."
+cwd = os.getcwd()
+os.chdir(git_path)
+call_and_check("git pull")
+
+print "Mapping GNOS repos and project IDs..."
+gnos_id_to_repo = {}
+gnos_id_to_project = {}
+json_files = glob('*/*.json')
+for jfile in json_files:
+    jfile = jfile.strip()
+
+    with open(jfile) as json_file:
+        json_data = json.load(json_file)
+        gnos_id   = json_data.get('gnos_id')
+        gnos_url  = json_data.get('gnos_repo')
+        proj_code = json_data.get('project_code')
+        if gnos_id:
+            gnos_url  = str(gnos_url[0])
+            gnos_repo = gnos_url.replace('https://gtrepo-','')
+            gnos_repo = gnos_repo.replace('.annailabs.com/','')
+            gnos_id_to_repo[gnos_id] = gnos_repo
+            gnos_id_to_project[gnos_id] = proj_code
+
+# Get the count for each queueing category
+print "Getting file counts..."
+file_count = {}
+jobs = glob('*jobs')
+for status in jobs:
+    files = glob(status+'/*.json')
+    count = len(files)
+    file_count[status] = count
+
+save_json('counts.json',file_count)
+
+# Get/save history data
+print "Getting count history..."
+hist_json = {}
+yest_json = yesterday_path + '/hist_counts.json'
+if os.path.isfile(yest_json):
+    with open(yest_json) as json_file:
+        hist_json = json.load(json_file)
+
+hist_json[today] = file_count
+save_json('hist_counts.json',hist_json)
+
+# Get timing information
+print "Getting timing data...";
+timing = {'download':{},'upload':{}}
+os.chdir(timing_path);
+files = glob('*.json.timing')
+for timing_file in files:
+    with open(timing_file) as infile:
+        csv_rows = csv.reader(infile)
+        for row in csv_rows:
+            if row[0] == 'gnos-id':
+                continue
+
+            gnos_id = row[0]
+            gnos_repo = gnos_id_to_repo.get(gnos_id)
+            if not gnos_repo:
+                break
+
+            nums = [float(t) for t in list(row)[1:12]]
+            (download_start,
+            download_stop,
+            download_delta,
+            upload_start,
+            upload_stop,
+            upload_delta,
+            workflow_start,
+            workflow_stop,
+            workflow_delta,
+            download_size,
+            upload_size) = nums
+
+            download_day = datetime.datetime.fromtimestamp(download_start).strftime('%Y-%m-%d')
+            download_rate = (download_size/download_delta)/1024
+            add_timepoint(timing,'download',gnos_repo,download_day,download_rate)
+            upload_day = datetime.datetime.fromtimestamp(upload_start).strftime('%Y-%m-%d')
+            upload_rate = (upload_size/upload_delta)/1024
+            add_timepoint(timing,'upload',gnos_repo,upload_day,upload_rate)
+
+save_json('timing.json',timing)
+
+# save our work
+os.chdir(reports)
+call_and_check("ln -sf "+today+" latest")
+
+
